b835f33fb0f6649a30332f666fdec447184d0d43
saraccy <sara910314@gmail.com>
2023-05-07 02:01:37 +0800

    add Header/Cart/Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
+import "./components/Styles/reset.css";
+import Header from './components/Header/Header.jsx'
 import StepProgress from './components/StepProgress/StepProgress.jsx'
 import AllStep from './components/Step/AllStep.jsx'
 import ProgressControl from './components/ProgressControl/ProgressControl.jsx'
 import styles from './App.module.css';
+import Cart from './components/Cart/Cart.jsx';
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   return (
-    <>
+    <div>
+      <Header/>
       {/* main */}
-        <main className="site-main">
+        <main className={styles.siteMain}>
           <div className={styles.mainContainer}>
 
-            {/* register */}
+            {/* register */} 
             <section className={styles.registerContainer} data-phase="1" data-total-price = "0">
-
-              {/* register-title */}
-                <StepProgress/>
-                
-              {/* register-form */}
-                <AllStep/>
+              
+              <StepProgress/>
+              <AllStep/>
             </section>
 
-            {/* progress-control */}
-              <ProgressControl/>
+            <Cart/>
+            <ProgressControl/>
           </div>
         </main>
-      </>
+        <Footer/>
+      </div>
     );
   }
 
