87b170e7251e417790d7eda360d38cd2956932dd
saraccy <sara910314@gmail.com>
2023-05-14 13:33:16 +0800

    adding dynamic effect

--- src/components/StepProgress/StepProgress.jsx
+++ src/components/StepProgress/StepProgress.jsx
@@ -1,32 +1,75 @@
 import styles from './StepProgress.module.css'
 import  PGCompleteIcon  from '../../icons/pg-complete.svg';
 
-function Step({stepPhase, stepNum, stepText}){
+function Step({stepPhase, number, stepText, stepNum}){
   return (
     <>
       <span className={styles.progressGroup} data-phase={stepPhase}>
-        <span className={styles.progressIcon}>
-          <span className={styles.progressNum}>{stepNum}</span>
+       
+        {(stepNum === Number(number) &&  Number(number) === 1) && 
+          (<span className={styles.progressIcon}> 
+            <span className={styles.progressOne}>{number}</span>
+            <img className={styles.pgComplete} src={PGCompleteIcon} alt=""/>
+           </span>) }
+
+        {(stepNum === Number(number) && (Number(number) ===
+        2 || Number(number) === 3)) &&
+        (<span className={styles.progressIcon}> 
+          <span className={styles.progressNum}>{number}</span>
+          <div className={styles.pgInProgress}></div>
+         </span>) }
+
+        {stepNum > Number(number) && 
+        (<span className={styles.progressIcon}> 
+          <span className={styles.completeNum}>&#10004;</span>
           <img className={styles.pgComplete} src={PGCompleteIcon} alt=""/>
-        </span>
+        </span>) }
+
+        {stepNum < Number(number) && 
+        (<span className={styles.progressIcon}> 
+          <span className={styles.UndoneNum}>{number}</span>
+          <div className={styles.pgUndone}></div>
+        </span>) }
+
         <span className="progress-label">{stepText}</span>
       </span>
+
+      { Number(number) !== 3  && (
+      <span className={styles.progressBar} data-order={number}></span>)}
+
     </>
   )
 }
 
-export default function StepProgress() {
+export default function StepProgress({stepNum}) {
   return (
     <>
       {/* register-title */}
       <h2 className={styles.registerTitle}>結帳</h2>
+
       {/* register-progress */}
       <section className={styles.progressContainer}>
-        <Step stepPhase="address" stepNum="1" stepText="寄送地址"/>
-        <span className={styles.progressBar} data-order={1} />
-        <Step stepPhase="shipping" stepNum="2" stepText="運送方式"/>
-        <span className={styles.progressBar} data-order={2} />
-        <Step stepPhase="credit-card" stepNum="3" stepText="付款資訊"/>
+
+        <Step 
+          stepPhase="address" 
+          number="1"
+          stepText="寄送地址"
+          stepNum={stepNum}
+        />
+        
+        <Step 
+          stepPhase="shipping" 
+          number="2"
+          stepText="運送方式"
+          stepNum={stepNum}
+        />
+
+        <Step 
+          stepPhase="credit-card" 
+          number="3"
+          stepText="付款資訊"
+          stepNum={stepNum}
+        />
       </section>
     </>
   )
