71b4415f1f4c01cab05e65d2f6fc79b9675e792b
saraccy <sara910314@gmail.com>
2023-08-05 17:35:08 +0800

    add onchange event of input and import ShippingContext

--- src/components/Main/Step/StepTwo/StepTwo.jsx
+++ src/components/Main/Step/StepTwo/StepTwo.jsx
@@ -1,9 +1,12 @@
 import styles from './StepTwo.module.css';
+import { useState, useContext } from 'react';
+import ShippingContext from '../../Context/ShippingContext';
 
-function Shipping({ text, period, price}) {
+function Shipping({value, isSelected ,text, period, price}) {
+  const {handleInputChange} = useContext(ShippingContext);
   return(
     <label className={styles.radioGroup}  data-price="0">
-      <input className={styles.shippingStandard} type="radio" name="shipping"  checked={true} />
+      <input className={styles.shippingStandard} type="radio" name="shipping" value={value} checked={isSelected} onChange={handleInputChange}/>
       <div className={styles.radioInfo}>
         <div className={styles.container}>
           <div className={text === "DHL 貨運" ? styles.dhlText : styles.text}>{text}</div>
@@ -18,16 +21,23 @@ function Shipping({ text, period, price}) {
 }
 
 function StepTwo() {
+  const {selectedOption} = useContext(ShippingContext);
   return(
     <form className={styles.formContainer} data-phase="shipping">
     <h3 className={styles.formTitle}>運送方式</h3>
     <section className={styles.formBody}>
       <Shipping 
+        value = "0"
+        isSelected={selectedOption === "0"}
+        // onChange = {handleInputChange}
         text="標準運送"
         period="約 3~7 個工作天"
         price="免費"
       />
       <Shipping 
+        value = "500"
+        isSelected={selectedOption === "500"}
+        // onChange = {handleInputChange}
         text="DHL 貨運"
         period="48 小時內送達"
         price="$500"
